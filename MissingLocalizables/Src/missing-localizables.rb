c0379b6b8b63ecdf0708908410ee36770db8b13f
Michał Cichoń <michal.cichon@codete.com>
2018-06-26 08:48:07 +0200

    MissingLocalizables: script

---
+++ MissingLocalizables/Src/missing-localizables.rb
@@ -0,0 +1,117 @@
+#!/usr/bin/env ruby
+
+LOCALIZABLE = "Localizable.strings"
+LPROJ = ".lproj"
+
+path = ARGV[0]
+base_lang = ARGV[1]
+
+def get_string(filepath)
+    f = File.open(filepath, "r")
+    str = f.read
+    f.close
+    return str
+end
+
+if path.nil? || base_lang.nil?
+    puts "Usage: \t\t" + __FILE__ + " <path_to_localizables> <base_language>"
+    puts "Example: \t" + __FILE__ + " ../../path/to/my/project/ en"
+    abort
+end
+
+base_file_path = path + base_lang + LPROJ + "/" + LOCALIZABLE
+
+if !File.exist?(base_file_path)
+    puts "Base file not found for: " + base_lang
+    abort
+end
+
+lint_cmd = "plutil -lint \"#{base_file_path}\""
+lint_result = `#{lint_cmd}`
+
+if !lint_result.include? "OK"
+    puts "Base file is incorrect"
+    puts lint_result
+    abort
+end
+
+base_str = get_string(base_file_path)
+base_hash = Hash.new
+base_ok = true
+
+a = base_str.split(";\n")
+
+a.each { |line|
+    t = line.split(/\s*=\s*/)
+    key = t[0].strip
+    value = t[1].strip
+    if base_hash.key?(key)
+        puts "💩 Duplicated key: #{key}"
+        base_ok = false
+    end
+    base_hash[key] = value
+}
+
+if base_ok 
+    puts "----\n✅ Base localizable OK"
+else
+    puts "----\n⛔️ Base localizable is not OK"
+end
+
+Dir.chdir(path)
+subdir_list = Dir["*"].reject{ |o| not File.directory?(o) or !o.include? LPROJ }
+
+subdir_list.each { |subdir|
+    next if subdir.include? base_lang
+    lang_path = path + subdir + "/" + LOCALIZABLE
+    # puts lang_path
+    next if !File.exist?(lang_path)
+    str = get_string(lang_path)
+    b = str.split(";\n")
+    sub_hash = Hash.new
+    b.each { |line|
+        t = line.split(/\s*=\s*/)
+        key = t[0].strip
+        value = t[1].strip
+        
+        if sub_hash.key?(key)
+            puts "💩 Duplicated key: #{key} for lang: " + subdir
+        end
+
+        if !base_hash.key?(key)
+            puts "👎 Key: " + key + " of " + subdir + " is not in the base language"
+        end
+
+        sub_hash[key] = value
+    }
+
+    missing_keys = Array.new
+    base_hash.each_key { |key|
+        if sub_hash[key].nil?
+            missing_keys.push(key)
+        end
+    }
+
+    if !missing_keys.empty?
+        puts "⛔️ Missing keys for language: " + subdir
+        missing_keys.each { |key|
+            puts "\t" + key
+        }
+        puts "----"
+    end
+}
+
+# a = base_str.scan(/"[^"]+"\s*=\s*".*"\s*;/m)
+
+# puts base_contents
+
+# a = base_str.split(/"[^"]+"\s*=\s*"[^"]*"\s*;/)
+
+# puts a
+
+# base_dict = Hash.new
+
+
+# File.open(base_file_path).each do |line|
+#     puts ":" + line
+# end
---
+++ README.md
@@ -0,0 +1,4 @@
+# Some useful scripts
+
+These are some of my scripts I use on my daily basis. Some of them are simple Ruby 💎 scripts or bash scripts made to simplify time-consuming or boring tasks.
+
